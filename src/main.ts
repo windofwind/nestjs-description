96bbd3bf9c326cb2fcafd52f9239d19fa2e7be10
dona <windofwind@gmail.com>
2023-01-03 15:45:28 +0900

    fix typo - server type

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const host = process.env.SERVER_HOST || '0.0.0.0';
   const port = Number(process.env.SERVER_PORT) || 3000;
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   await app.listen(port, host);
 }
