3f6839dce81ece7f177cbe0e855480dbb21af8a2
dona <windofwind@gmail.com>
2023-01-03 15:39:50 +0900

    update setting

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const host = process.env.SERVER_HOST;
+  const port = Number(process.env.SERVER_PORT);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(port, host);
 }
 bootstrap();
