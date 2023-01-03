ba0e2477a03be2c927dd93ce605e900644ab3963
dona <windofwind@gmail.com>
2023-01-03 15:44:58 +0900

    fix host port

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const host = process.env.SERVER_HOST;
-  const port = Number(process.env.SERVER_PORT);
+  const host = process.env.SERVER_HOST || '0.0.0.0';
+  const port = Number(process.env.SERVER_PORT) || 3000;
   const app = await NestFactory.create(AppModule);
 
   await app.listen(port, host);
